e41ee6afa3e612b9662506a5feb2ab1159db2543
Caio Cunha <160679697+caiodscunha@users.noreply.github.com>
2025-05-10 21:36:25 -0300

    chore: added dependecies and permissions for camera

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -81,4 +81,14 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx:25.1.3")
     implementation("com.google.firebase:firebase-auth-ktx:23.2.0")
 
+    //dependencias de câmera
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
+
+    //Zxing - Decodificador de QRcode
+    implementation("com.google.zxing:core3.3.3")
+
 }
